299517f4cdf6b2e319d6edeb9f4a225788cfd836
Vinay Bojja <65752906+vinay-bojja@users.noreply.github.com>
2023-01-02 17:22:05 -0800

    Update local.ts

--- src/appConfig/env/local.ts
+++ src/appConfig/env/local.ts
@@ -1,14 +1,14 @@
 const config = {
     NAME: process.env.NAME || "DollarManager",
-    MONGO_URL: process.env.MONGO_URL || "mongodb+srv://vinay:SideProject123@cluster0.hue9xxx.mongodb.net/?retryWrites=true&w=majority",
+    MONGO_URL: process.env.MONGO_URL || "", //enter mongodb URL
     PORT: process.env.PORT || 5000,
     BACKEND_URL: process.env.BACKEND_URL || "http://localhost:5000",
     FRONTEND_URL: process.env.FRONTEND_URL || "http://localhost:3000",
-    Gmail_User: process.env.GMAIL_USER || "vinay.bojja@viit.ac.in",
-    Gmail_Password: process.env.GMAIL_PASSWORD || "abc@12345",
+    Gmail_User: process.env.GMAIL_USER || "", //gmail
+    Gmail_Password: process.env.GMAIL_PASSWORD || "", //gmail password
     GOOGLE: {
-        GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID || "539808842398-vgv7qitl4p6i692ba6efkmmmggkl90vp.apps.googleusercontent.com",
-        GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET || "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"
+        GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID || "", //google clientId
+        GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET || "" //google secretId
     },
     CORS_OPTIONS: {
         WHITELISTDOMAINS: [
